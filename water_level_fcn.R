5dd12e9176191b7772af5331eebf42b98ac04483
Andrew Chien <andrewkb.chien@gmail.com>
2019-02-18 17:32:02 +0800

    Added feature to subset the sorted dataset by one day to create a smaller dataset for testing the WaterLevel.ipynb code.

--- water_level_fcn.R
+++ water_level_fcn.R
@@ -1,8 +1,10 @@
 SubsetByMonth <- function(timeseries.df, set.month) {
-  # Subsets a times series dataframe with a timestamp column called Date.and.Time into a month 
+  # Subsets a times series dataframe with a timestamp column called Date.and.Time into 
+  # a month 
   # 
   # Args:
-  #   timeseries.df: Time series dataframe that contains a date time column in the class of factor or character
+  #   timeseries.df: Time series dataframe that contains a date time column in the class 
+  #   of factor or character
   #   set.month: a string that is the format "YYYY-MM"
   #
   #   Returns:
@@ -34,5 +36,46 @@ SubsetByMonth <- function(timeseries.df, set.month) {
   #TODO check if set.month argument is in the valid range
   
   return (one.month)
+  
+}
+
+SubsetByDay <- function(timeseries.df, set.day) {
+  # Subsets a times series dataframe with a timestamp column called Date.and.Time into 
+  # a month 
+  # 
+  # Args:
+  #   timeseries.df: Time series dataframe that contains a date time column in the class  
+  #   of factor or character
+  #   set.day: a string that is the format "YYYY-MM-DD"
+  #
+  #   Returns:
+  #   one.day: A dataframe that has been subset-ed to a day.  
+  # Date: 18 Febuary 2019
+  # Author: Andrew Chien
+  # TODO(AndrewC): Need to add error handling for arguments
+  
+  #converts the Date.and.Time column into POSIXct Class
+  timeseries.df$Date.and.Time <- as.POSIXct(timeseries.df$Date.and.Time, 
+                                            format="%m/%d/%Y %H:%M:%S")
+  
+  #adds the string "00:00:00" to complete the date time as first second of the day
+  startdatetime <- paste(set.day, " 00:00:00", sep="")
+  
+  #calculates the last second of set.day
+  enddatetime <- (seq(as.Date(startdatetime), length=2, by="day") - 1)
+  enddatetime <- (enddatetime[-1] + 1)
+  
+  #converts the string into Date Class
+  startdatetime <- as.Date(startdatetime)
+  
+  #subsets timeseries.df to the set startdatetime and enddatetime range
+  one.day <- subset(timeseries.df,timeseries.df$Date.and.Time  >= startdatetime 
+                      & timeseries.df$Date.and.Time < enddatetime)
+  
+  # Error handling
+  #TODO check if set.month argument is the correct format
+  #TODO check if set.month argument is in the valid range
+  
+  return (one.day)
   
 }
